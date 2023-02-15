bc924779ec29b0b647b639307000dfc884f1b959
tanmay patil <tanmay.admirer@gmail.com>
2023-02-15 23:37:32 +0530

    core functionality ready with unpolished ui , added channel calculation formulae and distance calculation formulae.

--- app/src/main/java/com/example/codebreak/DetailInfo.java
+++ app/src/main/java/com/example/codebreak/DetailInfo.java
@@ -8,21 +8,39 @@ import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.anastr.speedviewlib.AwesomeSpeedometer;
 
 public class DetailInfo extends AppCompatActivity {
-    TextView textView;
+    TextView textView,text_ip,text_frequency,text_channel,text_rssi,text_mac,text_bssid,text_linkspeed,text_netid,distance;
     WifiManager wifiManager;
+    AwesomeSpeedometer awesomeSpeedometer;
+    Handler handler = new Handler();
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_info);
         textView=findViewById(R.id.t5);
+        text_ip=findViewById(R.id.t6);
+        text_frequency=findViewById(R.id.t7);
+        text_channel=findViewById(R.id.t8);
+        text_rssi=findViewById(R.id.t9);
+        text_mac=findViewById(R.id.t10);
+        text_bssid=findViewById(R.id.t11);
+        text_linkspeed=findViewById(R.id.t12);
+        text_netid=findViewById(R.id.t13);
+        distance=findViewById(R.id.t14);
+
+        awesomeSpeedometer=findViewById(R.id.awesomeSpeedometer);
+        awesomeSpeedometer.setSpeedAt(5);
 
 
 
-        getInfo();
+        handler.post(runnableCode);
     }
 
 
@@ -59,6 +77,37 @@ public class DetailInfo extends AppCompatActivity {
 
         String ssid1 = ""+ssid;
         textView.setText(ssid1);
+        text_ip.setText("IP Address: "+ip);
+        text_frequency.setText("Frequency: "+frequency);
+
+        int channel = 0;
+        if (frequency >= 2412 && frequency <= 2484) {
+            channel = (frequency - 2412) / 5 + 1;
+        } else if (frequency >= 5170 && frequency <= 5825) {
+            channel = (frequency - 5170) / 5 + 34;
+        }
+        text_channel.setText("Channel: "+channel);
+
+        text_rssi.setText("RSSI: "+rssi);
+        text_mac.setText("MAC: "+mac);
+        text_bssid.setText("BSSID: "+bssid);
+        text_linkspeed.setText("Link Speed: "+linkspeed);
+        text_netid.setText("Network ID: "+netid);
+
+        int referenceRssi = -40;
+        double distance_val = Math.pow(10, (referenceRssi - rssi) / (10 * 2.0));
+        distance.setText("Distance : "+distance_val+"m");
+        int signal_Strength = rssi+120;
+        awesomeSpeedometer.speedTo(signal_Strength);
 
     }
+
+    private Runnable runnableCode = new Runnable() {
+        @RequiresApi(api = Build.VERSION_CODES.Q)
+        @Override
+        public void run() {
+            getInfo();
+            handler.postDelayed(this, 500);
+        }
+    };
 }
